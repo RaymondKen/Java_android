c3f76b6a9b26948813870ff8bcd547da852b3fe1
R Kelven <kelven.raymond@epitech.eu>
2017-02-06 00:04:25 +0100

    last v
    inc

---
+++ Last version/app/src/main/java/com/todolist/chronos/activity_example/information_activity.java
@@ -0,0 +1,17 @@
+package com.todolist.chronos.activity_example;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Chronos on 05/02/2017.
+ */
+
+public class information_activity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstance)
+    {
+        super.onCreate(savedInstance);
+        setContentView(R.layout.informations);
+    }
+}
---
+++ Last version/app/src/main/java/com/todolist/chronos/activity_example/principalMain.java
@@ -0,0 +1,180 @@
+package com.todolist.chronos.activity_example;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.todolist.chronos.activity_example.db.TaskDbHelper;
+
+import java.util.ArrayList;
+
+import static android.R.attr.button;
+
+/**
+ * Created by Chronos on 24/01/2017.
+ */
+
+public class principalMain extends AppCompatActivity{
+    private static final String TAG = "Main activity";
+    private TaskDbHelper mHelper;
+    private ListView mTaskListView;
+    private ArrayAdapter<String> mAdapter;
+    private int amount = 0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mHelper = new TaskDbHelper(this);
+        mTaskListView = (ListView) findViewById(R.id.todo);
+        SQLiteDatabase db = mHelper.getReadableDatabase();
+        Cursor cursor = db.query(taskContract.TaskEntry.TABLE,
+                new String[]{taskContract.TaskEntry._ID, taskContract.TaskEntry.COL_TASK_TITLE},
+                null, null, null, null, null);
+        while(cursor.moveToNext()) {
+            int idx = cursor.getColumnIndex(taskContract.TaskEntry.COL_TASK_TITLE);
+            Log.d(TAG, "Task: " + cursor.getString(idx));
+        }
+        cursor.close();
+        db.close();
+        updateUI();
+    }
+
+    /*@Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }*/
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        final EditText boxEdit = new EditText(this);
+        mHelper = new TaskDbHelper(this);
+
+        switch (item.getItemId()) {
+            case R.id.add_option:
+                Log.d(TAG, "Add a new task");
+                AlertDialog dialog = new AlertDialog.Builder(this)
+                        .setTitle("Add a new task")
+                        .setMessage("What do you want to do next?")
+                        .setView(boxEdit)
+                        .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                String task = String.valueOf(boxEdit.getText());
+
+                                Log.d(TAG, "Task to add: " + task);
+                                SQLiteDatabase db = mHelper.getWritableDatabase();
+                                ContentValues values = new ContentValues();
+                                values.put(taskContract.TaskEntry.COL_TASK_TITLE, task);
+                                db.insertWithOnConflict(taskContract.TaskEntry.TABLE,
+                                        null,
+                                        values,
+                                        SQLiteDatabase.CONFLICT_REPLACE);
+                                db.close();
+                                updateUI();
+                            }
+                        })
+                        .setNegativeButton("Cancel", null)
+                        .create();
+                dialog.show();
+
+                return true;
+
+            case R.id.option:
+                Log.d(TAG, "Edit Options");
+                final Context context = this;
+                Intent intent = new Intent(context, linear_tab.class);
+                startActivity(intent);
+                return true;
+
+            case R.id.info:
+                Log.d(TAG, "See Information");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void updateUI() {
+        ArrayList<String> taskList = new ArrayList<>();
+        SQLiteDatabase db = mHelper.getReadableDatabase();
+        Cursor cursor = db.query(taskContract.TaskEntry.TABLE,
+                new String[]{taskContract.TaskEntry._ID, taskContract.TaskEntry.COL_TASK_TITLE},
+                null, null, null, null, null);
+        while (cursor.moveToNext()) {
+            int idx = cursor.getColumnIndex(taskContract.TaskEntry.COL_TASK_TITLE);
+            taskList.add(cursor.getString(idx));
+        }
+
+        if (mAdapter == null) {
+            mAdapter = new ArrayAdapter<>(this,
+                    R.layout.item_block,
+                    R.id.task_title,
+                    taskList);
+            mTaskListView.setAdapter(mAdapter);
+        } else {
+            mAdapter.clear();
+            mAdapter.addAll(taskList);
+            mAdapter.notifyDataSetChanged();
+        }
+
+        cursor.close();
+        db.close();
+
+        System.out.println(amount);
+    }
+
+    public void deleteTask(View view) {
+        View parent = (View) view.getParent();
+        TextView taskTextView = (TextView) parent.findViewById(R.id.task_title);
+        String task = String.valueOf(taskTextView.getText());
+        SQLiteDatabase db = mHelper.getWritableDatabase();
+        db.delete(taskContract.TaskEntry.TABLE,
+                taskContract.TaskEntry.COL_TASK_TITLE + " = ?",
+                new String[]{task});
+        db.close();
+        updateUI();
+        amount -= 1;
+    }
+
+    public void validTask(View view)
+    {
+        View parent = (View) view.getParent();
+        TextView taskTextView = (TextView) parent.findViewById(R.id.task_title);
+        String task = String.valueOf(taskTextView.getText());
+        SQLiteDatabase db = mHelper.getWritableDatabase();
+        db.delete(taskContract.TaskEntry.TABLE,
+                taskContract.TaskEntry.COL_TASK_TITLE + " = ?",
+                new String[]{task});
+        db.close();
+        updateUI();
+        amount += 1;
+    }
+
+    public void changeColor(View view)
+    {
+        view.setBackgroundColor(ContextCompat.getColor(this, R.color.green));
+        Log.d(TAG, "j'ai capter");
+    }
+
+}
